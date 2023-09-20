ed28fc1676c44e2cfe68a8b44c27d7f0fca119bd
rohanpuri81 <rohanpuri81@gmail.com>
2023-09-20 21:39:54 +0530

    Filtering feature of ecommerce website

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './Navigation/Nav'
+import Products from './Products/Products'
+import Recommended from './Recommended/Recommended'
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Products />
+      <Recommended />
+    </>
+  )
+}
+
+export default App
---
+++ src/Products/Products.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Products.css'
+
+const Products = () => {
+  return (
+    <>
+      <section className="card-container">
+        <sectionn className="card">
+          
+        </sectionn>
+      </section>
+    </>
+  )
+}
+
+export default Products
